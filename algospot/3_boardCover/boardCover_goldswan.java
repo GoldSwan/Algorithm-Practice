b8eb6b8758958a573424eec2b625e24552066ed3
GoldSwan <saltmenu@naver.com>
2019-12-29 19:21:36 +0900

    3주차 문제풀이 업데이트

---
+++ algospot/3_boardCover/boardCover_goldswan.java
@@ -0,0 +1,9 @@
+package algorithm.algospot;
+
+public class boardCover {
+
+	public static void main(String[] args){
+
+		System.out.println("Hello!");
+	}
+}
---
+++ programers/3_findPrimeNumbers/findPrimeNumbers_goldswan.java
@@ -0,0 +1,20 @@
+package algorithm.programers;
+
+class Solution {
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+public class bracketTransformation {
+
+	public static void main(String[] args) {
+
+		String numbers = "";
+		Solution sol = new Solution();
+		int answer = sol.solution(numbers);
+		System.out.println(answer);
+	}
+
+}
