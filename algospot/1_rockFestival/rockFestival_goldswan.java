8861f58aacd8841a5eff57b50badf9b0bcb30a03
GoldSwan <saltmenu@naver.com>
2019-12-29 19:12:01 +0900

    1주차 문제풀이 GoldSwan

---
+++ algospot/1_rockFestival/rockFestival_goldswan.java
@@ -0,0 +1,95 @@
+/********************************************************************
+작성일 : 2019-12-21
+작성자 : KSW
+문제 : 록 페스티벌
+출저 : 알고스팟
+풀이 : 메모리제이션 이용
+예상 시간복잡도 : O(N^2)
+TEST 결과 :  380ms~420ms
+*********************************************************************/
+
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.HashMap;
+
+public class rockFestival {
+
+	private static int[] CostArray;//비용배열
+	static Scanner sc = new Scanner(System.in);
+	static Vector<Double> vAnswers = new Vector<Double>();//답이 저장된 벡터
+	
+	public static void main(String[] args) {
+
+		//테스트 케이스 수 C
+		int C = sc.nextInt();
+
+		for (int i = 0; i < C; ++i) {
+
+			//공연장을 대여할 수 있는 날들의 수 N ( 1 <= N <= 1000)
+			int N = sc.nextInt();
+
+			//이미 섭외한 공연 팀의 수 L (1 <= L <= 1000, L <= N) 
+			int L = sc.nextInt();
+
+			CostArray = new int[N];//비용 배열 CostArray
+
+			for (int k = 0; k < N; ++k) {
+				int Cost = sc.nextInt();//모든 비용은 100 이하 자연수 입력
+				CostArray[k] = Cost;
+			}
+
+			// 알고리즘 동작
+			solution(N, L, CostArray);
+		}
+		
+		//정답 출력 소수점 11자리 까지
+		for(int i = 0; i < vAnswers.size(); ++i) {
+			System.out.format("%.11f",vAnswers.get(i));
+			System.out.println();
+		}
+	}
+	//최소 평균비용을 구하는 알고리즘
+	public static void solution(int N, int L, int[] CostArray) {
+
+		//첫번째 원소 포함하고 연속된 인덱스로 이루어진 비용 부분합 해시맵
+		HashMap<Integer, Integer> firstPartialSumMap = new HashMap<Integer, Integer>();
+
+		int firstPartialSumArray = 0;
+		int minPartialSum;
+		int partialSum;
+		double avgCost;
+		double minAvgCost = Double.MAX_VALUE;
+
+		//비용 배열 CostArray의 첫번째 인덱스인 0인덱스 값을 포함하고 연속된 인덱스로 이루어진 부분배열의 합을 미리 구하여 해시에 저장
+		for (int i = 0; i < N; i++) {
+			firstPartialSumArray += CostArray[i];
+			firstPartialSumMap.put(i + 1, firstPartialSumArray);
+		}
+		
+		//L 길이를 갖는 평균 비용 ~ N 길이를 갖는 비용 평균 비용 계산  
+		while (L <= N) {
+			
+			minPartialSum = Integer.MAX_VALUE;
+			
+			//비교할 첫번째 부분 배열합 저장		
+			partialSum = firstPartialSumMap.get(L);
+			minPartialSum = Math.min(partialSum, minPartialSum);
+			
+			for (int i = L+1; i <= N; i++) {
+				//해시맵으로 미리 계산된 값을 이용하여 부분합의 다음 인덱스의 값을 더하고 첫번째 부분합 인덱스의 값을 빼서 각 구간 부분합 계산 
+				partialSum = partialSum + CostArray[i - 1] - CostArray[i - L - 1];
+				//L 길이를 갖는 부분합 중 최소 부분합 비교
+				minPartialSum = Math.min(partialSum, minPartialSum);
+			}
+			
+			//평균 비용 계산
+			avgCost = (double) minPartialSum / L;
+			//최소 평균 비용 비교
+			minAvgCost = Math.min(avgCost, minAvgCost);
+			
+			L++;
+		}
+		//답안 추가
+		vAnswers.add(minAvgCost);
+	}
+}
---
+++ programers/1_mockTest/mockTest_goldswan.java
@@ -0,0 +1,178 @@
+/********************************************************************
+작성일 : 2019-12-21
+작성자 : KSW
+문제 : 모의고사
+출저 : 프로그래머스
+풀이 : 완전탐색
+예상 시간복잡도 : O(N)
+TEST 결과 :  모두통과
+*********************************************************************/
+
+import java.util.Arrays;
+import java.util.Vector;
+
+//풀이법1
+class Solution {
+    int[] supoza1 = {1,2,3,4,5};
+    int[] supoza2 = {2,1,2,3,2,4,2,5};
+    int[] supoza3 = {3,3,1,1,2,2,4,4,5,5};
+    int[] correctAnswerCount = {0,0,0};
+    int[] correctAnswerCountSort = {0,0,0};
+    int[] answer;
+    Vector<Integer> vAnswers = new Vector<Integer>();
+    
+    public int[] solution(int[] answers) {
+
+    	//각 수포자별 정답수 카운트
+        for(int i=0;i<answers.length;i++)
+        {
+            if(supoza1[i%(supoza1.length)]==answers[i]){
+                correctAnswerCount[0]++;
+            }
+            if(supoza2[i%(supoza2.length)]==answers[i]){
+                correctAnswerCount[1]++;
+            }
+            if(supoza3[i%(supoza3.length)]==answers[i]){
+                correctAnswerCount[2]++;
+            }
+        }
+        
+        for(int i=0;i<correctAnswerCount.length;i++)
+        {
+            //각 수표자별 정답수 복사
+            correctAnswerCountSort[i] = correctAnswerCount[i];
+        }
+        //정렬
+        Arrays.sort(correctAnswerCountSort);//퀵정렬 시간복잡도 N*lnN
+        
+        //정렬하였으므로 가장 마지막 인덱스 값이 최고 점수가 됨. 동점자가 있을 수 있으므로 마지막 인덱스 값과 일치하는 수포자 번호를 vAnswers 에 넣음.
+        for(int i=0;i<correctAnswerCount.length;i++)
+        {
+            if(correctAnswerCountSort[correctAnswerCountSort.length-1]==correctAnswerCount[i]){
+            	vAnswers.add(i+1);
+            }
+        }
+    	
+        //정답 배열 생성 및 복사
+        answer = new int[vAnswers.size()];
+    	        
+        for(int i=0;i<answer.length;i++)
+        {
+            answer[i] = vAnswers.get(i);
+        }
+        
+        return answer;
+    }
+}
+
+//풀이법2
+class Solution2 {
+
+	int[] supoza1 = {1,2,3,4,5};
+    int[] supoza2 = {2,1,2,3,2,4,2,5};
+    int[] supoza3 = {3,3,1,1,2,2,4,4,5,5};
+    int correctAnswerCount1 = 0;
+    int correctAnswerCount2 = 0;
+    int correctAnswerCount3 = 0;
+    int answerLenth;
+    int[] answer = new int[3];
+    int[] realAnswer;
+    
+    public int[] solution(int[] answers) {   	
+    	answerLenth = 0;
+    	
+    	//각 수포자별 정답수 카운트
+        for(int i=0;i<answers.length;i++){
+            if(supoza1[i%(supoza1.length)]==answers[i]){
+                correctAnswerCount1++;
+            }
+            if(supoza2[i%(supoza2.length)]==answers[i]){
+                correctAnswerCount2++;
+            }
+            if(supoza3[i%(supoza3.length)]==answers[i]){
+                correctAnswerCount3++;
+            }
+        }        
+        
+        //모든 경우수를 구하여 최대 점수 수포자 구하기
+        
+        //수포자1이 젤 많이 맞출 경우
+        if(correctAnswerCount1>correctAnswerCount2 && correctAnswerCount1>correctAnswerCount3) {
+        	answer[0] = 1;
+        	answerLenth++;
+        }
+        //수포자2가 젤 많이 맞출 경우
+        if(correctAnswerCount2>correctAnswerCount3 && correctAnswerCount2>correctAnswerCount1) {
+        	answer[0] = 2;
+        	answerLenth++;
+        }
+        //수포자3가 젤 많이 맞출 경우
+        if(correctAnswerCount3>correctAnswerCount2 && correctAnswerCount3>correctAnswerCount1) {
+        	answer[0] = 3;
+        	answerLenth++;
+        }
+        //수포자1, 수포자2가 동점이고 수포자3이 아래일 경우
+        if(correctAnswerCount1==correctAnswerCount2 && correctAnswerCount1>correctAnswerCount3 && correctAnswerCount2>correctAnswerCount3) {
+        	answer[0] = 1;
+        	answer[1] = 2;
+        	answerLenth++;
+        	answerLenth++;
+        }
+        //수포자2, 수포자3이 동점이고 수포자1이 아래일 경우
+        if(correctAnswerCount2==correctAnswerCount3 && correctAnswerCount2>correctAnswerCount1 && correctAnswerCount3>correctAnswerCount1) {
+        	answer[0] = 2;
+        	answer[1] = 3;
+        	answerLenth++;
+        	answerLenth++;
+        }
+        //수포자1, 수포자3이 동점이고 수포자2이 아래일 경우
+        if(correctAnswerCount1==correctAnswerCount3 && correctAnswerCount1>correctAnswerCount2 && correctAnswerCount3>correctAnswerCount2) {
+        	answer[0] = 1;
+        	answer[1] = 3;
+        	answerLenth++;
+        	answerLenth++;        	
+        }
+        //수포자1,2,3 이 동점일 경우
+        if(correctAnswerCount1==correctAnswerCount2 && correctAnswerCount2==correctAnswerCount3) {
+        	answer[0] = 1;
+        	answer[1] = 2;
+        	answer[2] = 3;
+        	answerLenth++;
+        	answerLenth++;
+        	answerLenth++;
+        }
+        
+        //정답 배열 생성 및 복사
+        realAnswer = new int[answerLenth];
+
+        for(int i=0;i<realAnswer.length;i++) {
+        	realAnswer[i] = answer[i];
+        }
+        return realAnswer;
+    }
+}
+
+
+public class mockTest {
+     
+	public static void main(String[] args) {
+		//int[] answers = {1,2,3,4,5};
+		int[] answers = {1,3,2,4,2};
+
+		Solution sol = new Solution();
+		int answer[] = sol.solution(answers);
+		
+		Solution2 sol2 = new Solution2();
+		int answers2[] = sol2.solution(answers);
+		
+		//풀이법1
+		for(int i=0;i<answer.length;i++) {
+			System.out.print(answer[i]+" ");
+		}
+		System.out.println();
+		//풀이법2
+		for(int i=0;i<answer.length;i++) {
+			System.out.print(answers2[i]+" ");
+		}
+	}
+}
