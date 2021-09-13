77cce0fe549362a819bee80c23810a723490d458
goldswan <saltmenu@naver.com>
2021-09-13 19:38:58 +0900

    Add 'P0047_TheHighestAndLowestRanksOfLottos' source code.

---
+++ programers/P0047_TheHighestAndLowestRanksOfLottos/src/algorithm/programers/TheHighestAndLowestRanksOfLottos.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] arrAnswer = new int[2];
+        HashMap<Integer, Integer> mapWinNums = new HashMap<Integer, Integer>();
+
+        for(int win_num : win_nums){
+			mapWinNums.put(win_num,1);
+        }
+
+        arrAnswer[0] = getMaxRank(lottos, mapWinNums);
+        arrAnswer[1] = getMinRank(lottos, mapWinNums);
+
+        return arrAnswer;
+    }
+    //최고순위
+    private int getMaxRank(int[] lottos, HashMap<Integer, Integer> mapWinNums){
+        int cnt = 0;
+        for(int num : lottos){
+			if(mapWinNums.get(num)!=null || num==0)
+                cnt++;
+        }
+        return (cnt == 6) ? 1 : (cnt == 5) ? 2 : (cnt == 4) ? 3 : (cnt == 3) ? 4 : (cnt == 2) ? 5 : 6;
+    }
+    //최저순위
+    private int getMinRank(int[] lottos, HashMap<Integer, Integer> mapWinNums){
+         int cnt = 0;
+         for(int num : lottos){
+			if(mapWinNums.get(num)!=null)
+                cnt++;
+         }
+        return (cnt == 6) ? 1 : (cnt == 5) ? 2 : (cnt == 4) ? 3 : (cnt == 3) ? 4 : (cnt == 2) ? 5 : 6;
+    }
+}
