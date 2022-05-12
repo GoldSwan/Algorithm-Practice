1bc3946489e599e318c61bdab339a46d750bc1b7
GoldSwan <saltmenu@naver.com>
2022-05-12 17:50:11 +0900

    Refactoring 'P0051_CardMatching' source code.

--- programers/P0051_CardMatching/src/algorithm/programers/CardMatching.java
+++ programers/P0051_CardMatching/src/algorithm/programers/CardMatching.java
@@ -1,65 +1,63 @@
 package algorithm.programers;
 import java.util.*;
 
-class Pair{
-	int x;
-	int y;
-	int move;
-	
-	public Pair(int x, int y, int move){
-		this.x =x;
-		this.y =y;
-		this.move = move;
-	}
-}
-
 class Solution {
-	private List<String> orders;
-	private int[] dx = {1,-1,0,0};
-	private int[] dy = {0,0,1,-1}; 
-	public int solution(int[][] board, int r, int c) {		 
+	private List<String> arrListCases;
+	private final int[][] arrXY = {{1,0},{-1,0},{0,1},{0,-1}};
+	private class Point{
+		private int x;
+		private int y;
+		private int move;
+
+		public Point(int x, int y, int move){
+			this.x =x;
+			this.y =y;
+			this.move = move;
+		}
+	}
+
+	public int solution(int[][] board, int r, int c) {
 		 int numCardCount =0;
 		 int[] arrCards = {};
-		 
+		 int min = Integer.MAX_VALUE;
+
 		 numCardCount =  getNumCardCount(board);
 		 arrCards = getArrCards(numCardCount);
 
 		 //순서를 고려한 카드 조합(순열) 경우의 수 구하기
-		 orders = new ArrayList<>();
+		 arrListCases = new ArrayList<>();
 		 permutation("", 0, arrCards);
-		 
-		 int min = Integer.MAX_VALUE;
-		 for(String comb : orders) {
-			 String[] order = comb.split("");
-			 
-			 int total_move =0;
+
+		 for(String strCase : arrListCases) {
+			 String[] arrCase = strCase.split("");
+
+			 int numTotalMove =0;
 			 int[] pos = new int[2];
 			 pos[0] =r;
-			 pos[1] =c;			
-			 
-			 int[][] copy_board  = new int[4][4];
+			 pos[1] =c;
+
+			 int[][] arrCopyBoard  = new int[4][4];
+
 			 for(int i=0; i<4; i++) {
-				 for(int j=0; j<4; j++) {
-					 copy_board[i][j] = board[i][j];
-				 }
+				 System.arraycopy(board[i], 0, arrCopyBoard[i], 0, 4);
 			 }
-			 
-			 for(String target_card : order) {
-				 int card_num = Integer.parseInt(target_card);
 
-				 total_move += cardSearch(pos, card_num, copy_board);				 
-				 copy_board[pos[0]][pos[1]] = 0;
-				 total_move += 1;
+			 for(String strTargetCard : arrCase) {
+				 int numCard = Integer.parseInt(strTargetCard);
 
-				 total_move += cardSearch(pos, card_num, copy_board);
-				 copy_board[pos[0]][pos[1]] = 0;
-				 
-				 total_move += 1;
+				 numTotalMove += cardSearch(pos, numCard, arrCopyBoard);
+				 arrCopyBoard[pos[0]][pos[1]] = 0;
+				 numTotalMove += 1;
+
+				 numTotalMove += cardSearch(pos, numCard, arrCopyBoard);
+				 arrCopyBoard[pos[0]][pos[1]] = 0;
+
+				 numTotalMove += 1;
 			 }
-			 
-			 min = Math.min(min, total_move);
+
+			 min = Math.min(min, numTotalMove);
 		 }
-		 
+
 		 return min;
 	}
 
@@ -70,11 +68,11 @@ class Solution {
 				if(board[i][j] != 0) {
 					numCardCount ++;
 				}
-			}	
+			}
 		 }
 		return numCardCount/2;
 	}
-	
+
 	private int[] getArrCards(int numCardCount) {
 		 int[] arrCards = new int[numCardCount];
 		 for(int i=0; i<numCardCount; i++) {
@@ -82,83 +80,81 @@ class Solution {
 		 }
 		 return arrCards;
 	}
-	
-	private void permutation(String comb, int numDepth,int[] arrCards) {
+
+	private void permutation(String strCase, int numDepth, int[] arrCards) {
 		if(arrCards.length == numDepth) {
-			orders.add(comb);
-			return ;
+			arrListCases.add(strCase);
+			return;
 		}
-		
+
 		for(int i=0; i<arrCards.length; i++) {
 			int num = arrCards[i];
-			if(!comb.contains(""+num)) {
-				permutation(comb+num, numDepth+1, arrCards);
+			if(!strCase.contains(""+num)) {
+				permutation(strCase+num, numDepth+1, arrCards);
 			}
 		}
-		
 	}
-	
-	private int cardSearch(int[] pos, int target_card, int[][] copy_board) {
-		Queue<Pair> q = new LinkedList<>();
+
+	private int cardSearch(int[] pos, int numTargetCard, int[][] arrCopyBoard) {
+		Queue<Point> q = new LinkedList<>();
 		boolean[][] check = new boolean[4][4];
 		int x= pos[0];
 		int y= pos[1];
-		
+
 		check[x][y] = true;
-		q.add(new Pair(x,y,0));
+		q.add(new Point(x,y,0));
 		while(!q.isEmpty()) {
-			Pair next = q.poll();
+			Point next = q.poll();
 			int px = next.x;
 			int py = next.y;
 			int move =next.move;
-			
-			if(copy_board[next.x][next.y] == target_card) {
+
+			if(arrCopyBoard[next.x][next.y] == numTargetCard) {
 				pos[0] = next.x;
 				pos[1] = next.y;
 				return move;
 			}
-			
+
 			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-				
+				int nx = px + arrXY[i][0];
+				int ny = py + arrXY[i][1];
+
 				if(nx<0 || ny<0 || nx>3 || ny>3) continue;
 				if(check[nx][ny]) continue;
-				
+
 				check[nx][ny] = true;
-				q.add(new Pair(nx,ny, move+1));
+				q.add(new Point(nx,ny, move+1));
 			}
-			
+
 			for(int i=0; i<4; i++) {
-				Pair res = checkRoute(px,py, i, copy_board);
+				Point res = checkRoute(px,py, i, arrCopyBoard);
 				int nx = res.x, ny =res.y;
-				
+
 				if(nx==x && ny ==y) continue;
 				if(check[nx][ny]) continue;
-				
+
 				check[nx][ny] = true;
-				q.add(new Pair(nx,ny, move+1));
+				q.add(new Point(nx,ny, move+1));
 			}
 		}
 		return 0;
-		
-		
-		
+
+
+
 	}
 
-	private Pair checkRoute(int x, int y, int direction, int[][] copy_board) {
-		x += dx[direction];
-		y += dy[direction];
-		
+	private Point checkRoute(int x, int y, int direction, int[][] copyBoard) {
+		x += arrXY[direction][0];
+		y += arrXY[direction][1];
+
 		while(x >= 0 && x < 4 && y >= 0 && y < 4) {
-			if(copy_board[x][y] != 0) return new Pair(x,y,0);
-			
-			x += dx[direction];
-			y += dy[direction];
+			if(copyBoard[x][y] != 0) return new Point(x,y,0);
+
+			x += arrXY[direction][0];
+			y += arrXY[direction][1];
 		}
 
-		return new Pair(x-dx[direction],y-dy[direction],0);	
-		
+		return new Point(x-arrXY[direction][0],y-arrXY[direction][1],0);
 	}
 }
 
@@ -171,7 +167,7 @@ public class CardMatching {
 		int r = 1;
 		int c = 0;
 		int answer = 0;
-		 
+
 		answer = solution.solution(board, r, c);
 		System.out.println("answer:"+ answer);
 	}
