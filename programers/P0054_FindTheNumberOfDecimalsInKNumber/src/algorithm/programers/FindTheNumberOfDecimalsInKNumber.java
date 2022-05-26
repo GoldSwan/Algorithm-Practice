efe63f025bb78c392d5616e0292a11f805ea1108
GoldSwan <saltmenu@naver.com>
2022-05-26 10:39:38 +0900

    Fix 'P0054_FindTheNumberOfDecimalsInKNumber' source code.

--- programers/P0054_FindTheNumberOfDecimalsInKNumber/src/algorithm/programers/FindTheNumberOfDecimalsInKNumber.java
+++ programers/P0054_FindTheNumberOfDecimalsInKNumber/src/algorithm/programers/FindTheNumberOfDecimalsInKNumber.java
@@ -2,21 +2,39 @@ package algorithm.programers;
 
 class Solution {
     public int solution(int n, int k) {
-        int answer = -1;
+        int cntPrimeNumbers = 0;
         String strDecimalNum = getKDecimalNum(n, k);
-        System.out.println(strDecimalNum);
-        return answer;
+        //System.out.println(strDecimalNum);
+        String[] arrSplit = strDecimalNum.split("0");
+        for(String strNum : arrSplit) {
+        	if(strNum.equals(""))
+        		continue;
+        	if(isPrimeNumber(Long.parseLong(strNum))) {
+        		cntPrimeNumbers++;
+        	}
+        }
+
+        return cntPrimeNumbers;
     }
     public String getKDecimalNum(int n, int k) {
     	StringBuilder sb = new StringBuilder();
-    	
+
     	while(n>0) {
     		int remain = n%k;
     		sb.append(remain);
     		n /= k;
     	}
-    	
+
     	return sb.reverse().toString();
+    }
+
+    public boolean isPrimeNumber(long num) {
+    	if(num==1) return false;
+
+    	for(long i = 2 ; i*i <= num ; i++) {
+    		if(num % i == 0) return false;
+    	}
+    	return true;
     }
 }
 
@@ -24,10 +42,10 @@ public class FindTheNumberOfDecimalsInKNumber {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int n = 11;
+		int n = 437674;
 		int k = 3;
-		solution.solution(n, k);
-
+		int cntPrimeNumbers = solution.solution(n, k);
+		System.out.println("cntPrimeNumbers:"+cntPrimeNumbers);
 	}
 
 }
