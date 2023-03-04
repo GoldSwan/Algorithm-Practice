4aa6ca2ded3e01b171b51ad0c35a39cbe65d84ba
goldswan <saltmenu@naver.com>
2023-03-04 22:37:59 +0900

    Fix 'L0001' Refactoring, Add comment, Add input2

--- leetcode/L0001_TaskScheduler/src/algorithm/leetcode/TaskScheduler.java
+++ leetcode/L0001_TaskScheduler/src/algorithm/leetcode/TaskScheduler.java
@@ -3,46 +3,42 @@ import java.util.*;
 
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        var hashMap = createJobCntMap(tasks);
-        var maxKeyList = new ArrayList<Character>(hashMap.keySet());
-        Collections.sort(maxKeyList, (key1, key2) -> hashMap.get(key2).compareTo(hashMap.get(key1)));//내림차순
-        int numMinHour = 0;
-        
-        numMinHour = (hashMap.get(maxKeyList.get(0))-1) * (n+1);
-        
-        int maxTask = hashMap.get(maxKeyList.get(0));
+        var taskCntMap = createTaskCntMap(tasks);
+        var taskCntKeyList = new ArrayList<Character>(taskCntMap.keySet());
 
-        for(char task : maxKeyList){
-            int taskcnt = hashMap.get(task);
-            if(taskcnt == maxTask)
-                numMinHour += 1;
+        Collections.sort(taskCntKeyList, (key1, key2) -> taskCntMap.get(key2).compareTo(taskCntMap.get(key1)));//내림차순
+
+        int minTaskHour = (taskCntMap.get(taskCntKeyList.get(0))-1) * (n+1);
+        int maxTaskCnt = taskCntMap.get(taskCntKeyList.get(0));
+
+        int remain = 0;//가장 개수가 많은 task와 동일한 개수의 task를 찾아 나머지에 더해준다.
+        for(char task : taskCntKeyList){
+            int taskCnt = taskCntMap.get(task);
+            if(taskCnt == maxTaskCnt)
+                ++remain;
         }
 
-        if(numMinHour < tasks.length)
-            numMinHour = tasks.length;
+        minTaskHour += remain;
 
-        return numMinHour;
+        //remain에 가장 많은 task가 아닌 다른 task가 배치될 경우 tasks.length을 반환. 아래 input2 참고
+        return minTaskHour < tasks.length ? tasks.length : minTaskHour;
     }
 
-    public Map<Character, Integer> createJobCntMap(char[] tasks){
-        Map<Character, Integer> hashMap = new HashMap<>();
+    public Map<Character, Integer> createTaskCntMap(char[] tasks){
+        Map<Character, Integer> taskCntMap = new HashMap<>();
 
-        for(char task : tasks){
-            if(hashMap.containsKey(task)){
-                hashMap.put(task, hashMap.get(task)+1);
-                continue;
-            }
-            hashMap.put(task, 1);
-        }
-        return hashMap;
+        for(char task : tasks)
+            taskCntMap.put(task, taskCntMap.getOrDefault(task, 0)+1);
+
+        return taskCntMap;
     }
 }
 
 public class TaskScheduler {
     public static void main(String[] args) throws Exception {
         var solution = new Solution();
-        char[] tasks  = {'A','A','A','B','B','B'};
-        int n = 2;
+        char[] tasks  = {'A','A','A','B','B','B'}; int n = 2;//input1
+        //char[] tasks  = {'A','A','A','B','B','B', 'C', 'C', 'D', 'E', 'F', 'G', 'H'}; int n = 4;//input2
         int numMinHour = solution.leastInterval(tasks, n);
         System.out.println("numMinHour:"+numMinHour);
     }
