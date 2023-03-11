0e8a14718b89a30f1a33fa186f57dec2febfeb4d
goldswan <saltmenu@naver.com>
2023-03-11 21:30:51 +0900

    Fix 'P0036' Refactoring.

--- programers/P0036_CreateLargeNumber/src/algorithm/programers/CreateLargeNumber.java
+++ programers/P0036_CreateLargeNumber/src/algorithm/programers/CreateLargeNumber.java
@@ -2,18 +2,15 @@ package algorithm.programers;
 
 class Solution {
 	public String solution(String number, int k) {
-
 		String answer = "";
 		int index = 0;
 		int currentDigitNumber = number.length() - k;//현재 십진수의 자리수
-		
+		int LargeNumLength = currentDigitNumber;//가장 큰 수의 최대 자리수
 		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < number.length() - k; i++) {
-			
+
+		for (int i = 0; i < LargeNumLength; i++) {
 			char max = '0';
-			
-			for(int j = index ; j + currentDigitNumber <= number.length();j++){
+			for(int j = index ; j + currentDigitNumber <= number.length() ; j++){
 				//(j+1) : 선택한 index까지의 number 갯수
 				//(currentDigitNumber-1) : 선택한 index의 오른쪽에 있는 숫자들이 최소 있어야 할 개수
 				//둘을 합하면 j + currentDigitNumber <= number의 숫자 개수가 되어야함.
@@ -22,14 +19,11 @@ class Solution {
 					index = j + 1;
 				}
 			}
-			
 			sb.append(max);
-			
 			currentDigitNumber--;
 		}
-		
+
 		answer = sb.toString();
-		
 		return answer;
 	}
 }
