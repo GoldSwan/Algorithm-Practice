da291b92e1607289fb5659862ebec9e8234b30af
goldswan <saltmenu@naver.com>
2023-03-11 21:43:42 +0900

    Fix 'P0036' Refactoring.

--- programers/P0036_CreateLargeNumber/src/algorithm/programers/CreateLargeNumber.java
+++ programers/P0036_CreateLargeNumber/src/algorithm/programers/CreateLargeNumber.java
@@ -10,10 +10,13 @@ class Solution {
 
 		for (int i = 0; i < LargeNumLength; i++) {
 			char max = '0';
-			for(int j = index ; j + currentDigitNumber <= number.length() ; j++){
+			int maxIndex = number.length() - currentDigitNumber;
+			for(int j = index ; j <= maxIndex ; j++){
 				//(j+1) : 선택한 index까지의 number 갯수
 				//(currentDigitNumber-1) : 선택한 index의 오른쪽에 있는 숫자들이 최소 있어야 할 개수
 				//둘을 합하면 j + currentDigitNumber <= number의 숫자 개수가 되어야함.
+				//j <= currentDigitNumber - number.length() 이므로
+				//반복문이 돌때마다 1)length()를 호출하여 문자열 길이를 계산하는 연산과 2)상수 연산을 빼기 위해 우측 연산을 maxIndex 변수에 담음.
 				if (max < number.charAt(j)) {
 					max = number.charAt(j);
 					index = j + 1;
