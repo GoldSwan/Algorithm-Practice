6dbf198e4ef4d32a0cc043b89e0ea5b2c3599575
goldswan <saltmenu@naver.com>
2023-04-24 20:12:49 +0900

    Fix 'L0005' Code Refactoring.

--- leetcode/L0005_Permutations/src/algorithm/leetcode/Permutations.java
+++ leetcode/L0005_Permutations/src/algorithm/leetcode/Permutations.java
@@ -4,23 +4,25 @@ import java.util.*;
 class Solution {
     private final List<List<Integer>> result = new ArrayList<>();
     private final List<Integer> list = new ArrayList<>();
+    private final int VISITED = -11;
     public List<List<Integer>> permute(int[] nums) {
-        final int[] visited = new int[nums.length];
-        createPermutation(nums, visited, 0);
+        //final int[] visited = new int[nums.length];
+        createPermutation(nums, 0);
         return result;
     }
 
-    public void createPermutation(int[] nums, int[] visited, int depth){
+    public void createPermutation(int[] nums, int depth){
         if(depth == nums.length){
             result.add(new ArrayList<>(list));//ArrayList 생성자를 이용하여 list 깊은 복사 후 result에 넣음
             return;
         }
         for(int i = 0 ; i < nums.length ; i++){
-            if(visited[i] == 0){
+            if(nums[i] != VISITED){
                 list.add(nums[i]);
-                visited[i] = 1;
-                createPermutation(nums, visited, depth + 1);
-                visited[i] = 0;
+                int prev = nums[i];
+                nums[i] = VISITED;
+                createPermutation(nums, depth + 1);
+                nums[i] = prev;
                 list.remove(list.size()-1);
             }
         }
