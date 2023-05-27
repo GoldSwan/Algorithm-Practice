e1c8a31c9901881622f7fd0dff3917abfa1c320c
goldswan <saltmenu@naver.com>
2023-05-27 12:25:22 +0900

    Add 'P0063' Code.

--- programers/P0063_MakeTwoQueueSumSame/src/algorithm/programers/MakeTwoQueueSumSame.java
+++ programers/P0063_MakeTwoQueueSumSame/src/algorithm/programers/MakeTwoQueueSumSame.java
@@ -1,40 +1,87 @@
 package algorithm.programers;
+import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int minWork = -1;//최소 작업
-        int sumAll = 0;//전체 합
-        int sumTarget = 0;//목표 합
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        int minWork = -1;
+        long sumAll = 0;
+        long q1Sum = 0;
+        long q2Sum = 0;
 
         for(int num : queue1){
             sumAll+=num;
+            q1Sum+=num;
+            q1.add(num);
         }
         for(int num : queue2){
             sumAll+=num;
+            q2Sum+=num;
+            q2.add(num);
         }
 
-        if(sumAll%2 == 1){
-            return minWork;
-        }
-        
-        sumTarget = sumAll/2;
-
-        //생각
-        //1.두 큐의 합을 구한다.
-        //2.합이 큰 쪽 큐를 뽑아서 작은 쪽 큐에 넣는다.
-        //4.이 작업을 반복한다.
-        //무한정 작업할 수 없으니 중단 시점을 어떻게 정할지? 생각 필요
-        //중단 CASE 1. 하나의 숫자가 너무 커서 큐를 옮겨도 하나의 숫자 합이 더 큰경우
-
+        minWork = GetMinWork(q1, q2, q1Sum, q2Sum, sumAll);
         return minWork;
+    }
+
+    public int GetMinWork(Queue<Integer> q1 , Queue<Integer> q2, long q1Sum, long q2Sum, long sumAll){
+        int work = 0;
+        int worstLoopSize = q1.size() * 3;//최악의 루프 사이즈
+
+        //전체 합이 홀수인 경우는 두 큐의 합이 같을 수 없으므로 -1 리턴
+        if(sumAll%2 == 1){
+            return -1;
+        }
+        //이미 두 큐의 합이 같으면 작업을 할 필요가 없으므로 0 리턴
+        if(q1Sum == q2Sum){
+            return 0;
+        }
+
+        while(true){
+            //최악의 루프가 실행될 때 시간초과를 빠져나오기 위하여 추가 (테스트케이스11, 28)
+            if(work > worstLoopSize){
+                return -1;
+            }
+            //두 큐의 합이 같으면 루프문을 빠져나온다.
+            if(q1Sum == q2Sum){
+                break;
+            }
+            //큐가 한개라도 비게 되면 두 큐의 합이 같을 수 없으므로 -1 리턴
+            if(q1.isEmpty() || q2.isEmpty()){
+                return -1;
+            }
+            //두 큐 중 합이 더 큰 큐의 수를 뽑아 반대편 큐에 넣고 합을 계산하여 반영
+            if(q1Sum > q2Sum){
+                int num = q1.poll();
+                q2.add(num);
+                q1Sum -= num;
+                q2Sum += num;
+                work++;
+            }
+            else if(q1Sum < q2Sum){
+                int num = q2.poll();
+                q1.add(num);
+                q2Sum -= num;
+                q1Sum += num;
+                work++;
+            }
+        }
+
+        return work;
     }
 }
 
 public class MakeTwoQueueSumSame {
     public static void main(String[] args) throws Exception {
         var Solution = new Solution();
-        int[] queue1 = {3, 2, 7, 2};
-        int[] queue2 = {4, 6, 5, 1};
+        //int[] queue1 = {3, 2, 7, 2}; int[] queue2 = {4, 6, 5, 1};
+        //int[] queue1 = {1, 2, 1, 2}; int[] queue2 = {1, 10, 1, 2};
+        //int[] queue1 = {1, 1, 1}; int[] queue2 = {1, 1, 1};
+        int[] queue1 = {1, 1, 1, 1, 1, 2}; int[] queue2 = {1, 1, 1, 1, 1, 4};
+        //시간초과 테스트 케이스 : q2 29만9999개가 모두 q1로 넘어가고 q2의 59만 9999개가 q1으로 넘어간 경우
+        //int[] queue1 = {1, 1, 1, 1, 1, ... };/30만개
+        //int[] queue2 = {1, 1, 1, 1, 1, ..., 599999 ,1};//30만개
 
         int answer = Solution.solution(queue1, queue2);
         System.out.println(answer);
