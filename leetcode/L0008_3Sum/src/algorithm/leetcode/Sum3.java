d09d9218a32907ae3167652feaf905bddd291795
goldswan <saltmenu@naver.com>
2023-06-17 21:25:16 +0900

    Fix 'L0008' Code.

--- leetcode/L0008_3Sum/src/algorithm/leetcode/Sum3.java
+++ leetcode/L0008_3Sum/src/algorithm/leetcode/Sum3.java
@@ -1,6 +1,35 @@
 package algorithm.leetcode;
 import java.util.*;
 
+//참고 : https://leetcode.com/problems/3sum/solutions/3109452/c-easiest-beginner-friendly-sol-set-two-pointer-approach-o-n-2-logn-time-and-o-n-space/
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        output.addAll(set);
+        return output;
+    }
+}
+
+/*
 class Solution {
     //완전탐색풀이 : 시도 했더니 시간초과 에러 남
     public List<List<Integer>> threeSum(int[] nums) {
@@ -35,6 +64,7 @@ class Solution {
         return lists;
     }
 }
+ */
 
 public class Sum3 {
     public static void main(String[] args) throws Exception {
